316d86b58a6a701fa6bb92499bdae87dd8e38d00
William Krause <william.k.krause@gmail.com>
2017-10-07 15:41:27 -0400

    remove depricated method

--- android/src/main/java/co/apptailor/googlesignin/RNGoogleSigninPackage.java
+++ android/src/main/java/co/apptailor/googlesignin/RNGoogleSigninPackage.java
@@ -22,10 +22,10 @@ public class RNGoogleSigninPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
+    // @Override
+    // public List<Class<? extends JavaScriptModule>> createJSModules() {
+    //     return Collections.emptyList();
+    // }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
